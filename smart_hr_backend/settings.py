3e5ce2e7f1f8719724ba690bae550ef754fe9488
Thanushi Paranagama <thanushiparanagama@gmail.com>
2025-10-27 13:23:22 +0530

    new init final

--- smart_hr_backend/settings.py
+++ smart_hr_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-zf7*($f1hgnm3!azc#1v_x+u@009!wbl9o%43cq4-begl4&kph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.7']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -145,12 +145,7 @@ MEDIA_ROOT = BASE_DIR / 'media'  # ADD THIS
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS Settings - ADD THIS SECTION
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://192.168.1.7:3000",  # Add your mobile app's domain
-    "http://192.168.1.7:8000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
     'DELETE',
